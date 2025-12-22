17f46dd7819200b563fc941b9561595e83651640
MUSEr60406 <sammy60406test@gmail.com>
2025-12-22 06:30:35 +0000

    2025年12月22日 下午2:30

--- test.cpp
+++ test.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define pii pair<int,int>
 #define pll pair<long,long>
 #define ll long long
@@ -7,7 +7,12 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cout << 55;
+    int Case = 1;
+    while(true)
+    {
+        vector<vector<int>> Map(101, vector<int>());
+        vector<int> node;
+    }
 
     return 0;
 }
