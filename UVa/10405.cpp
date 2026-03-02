b1d696f28c052bb98bbc86da15b2c3d020f5a601
MUSEr60406 <sammy60406test@gmail.com>
2026-03-02 16:24:51 +0000

    2026年3月3日 上午12:24

--- UVa/10405.cpp
+++ UVa/10405.cpp
@@ -8,6 +8,11 @@ int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     string s1, s2;
+    while(cin >> s1 >> s2)
+    {
+        if(s1[0] == s2[0])
+            dp[1][1] = 1;
+    }
 
     return 0;
 }
