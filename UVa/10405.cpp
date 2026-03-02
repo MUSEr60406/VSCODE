d569455aa3a40eee1e205f9f5c427960e0d77bc7
MUSEr60406 <sammy60406test@gmail.com>
2026-03-02 16:26:19 +0000

    2026年3月3日 上午12:26

--- UVa/10405.cpp
+++ UVa/10405.cpp
@@ -10,8 +10,16 @@ int main()
     string s1, s2;
     while(cin >> s1 >> s2)
     {
-        if(s1[0] == s2[0])
-            dp[1][1] = 1;
+        for(int i = 1 ; i < s1.length() ; i++)
+        {
+            for(int j = 1 ; j < s2.length() ; j++)
+            {
+                if(s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max({dp[i - 1][j - 1]}, dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
     }
 
     return 0;
