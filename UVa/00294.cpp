280210ded600c9711cb5991ba598bc19d3bf3668
MUSEr60406 <sammy60406test@gmail.com>
2026-03-02 10:53:59 +0000

    2026年3月2日 下午6:53

--- UVa/00294.cpp
+++ UVa/00294.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    //建質數表
     vector<bool> isprime(31700, true);
     vector<int> prime;
     for(int i = 2 ; i <= 31700 ; i++)
