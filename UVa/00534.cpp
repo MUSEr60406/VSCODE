2170b7d671154ab421996fa648124588fcf85bd1
MUSEr60406 <sammy60406test@gmail.com>
2026-03-04 09:04:23 +0000

    2026年3月4日 下午5:04

--- UVa/00534.cpp
+++ UVa/00534.cpp
@@ -7,7 +7,11 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n;
+    int n ,count = 0;
+    while(cin >> n && n != 0)
+    {
+
+    }
 
     return 0;
 }
