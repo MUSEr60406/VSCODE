04d09fd429c91abc8a5751bb7f5db0544e6aee8f
MUSEr60406 <sammy60406test@gmail.com>
2026-03-04 09:08:22 +0000

    2026年3月4日 下午5:08

--- UVa/00534.cpp
+++ UVa/00534.cpp
@@ -8,9 +8,16 @@ int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n ,count = 0;
+    int M[201][201];
     while(cin >> n && n != 0)
     {
         vector<pii> stone;
+        for(int i = 0 ; i < n ; i++)
+        {
+            int x, y;
+            cin >> x >> y;
+            stone.push_back({x, y});
+        }
     }
 
     return 0;
