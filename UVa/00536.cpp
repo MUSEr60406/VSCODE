0fb2052a85f23097657ddfa217d6634829472040
MUSEr60406 <sammy60406test@gmail.com>
2026-03-04 13:35:30 +0000

    2026年3月4日 下午9:35

--- UVa/00536.cpp
+++ UVa/00536.cpp
@@ -3,14 +3,17 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-
+void f(string front, string middle)
+{
+    char root = front[0];
+}
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     string front, middle;
-    while(cin >> s1 >> s2)
+    while(cin >> front >> middle)
     {
-
+        f(front, middle);
     }
 
     return 0;
