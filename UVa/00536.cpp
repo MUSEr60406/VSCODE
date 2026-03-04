31145a705592b8a57f630bd08631ed2ffd3d15c6
MUSEr60406 <sammy60406test@gmail.com>
2026-03-04 13:51:56 +0000

    2026年3月4日 下午9:51

--- UVa/00536.cpp
+++ UVa/00536.cpp
@@ -6,17 +6,7 @@ using namespace std;
 void f(string front, string middle)
 {
     char root = front[0];
-    for(int i = 0 ; i < middle.size() ; i++)
-    {
-        if(middle[i] == root)
-        {
-            if(i != 0) //代表有左子樹
-                f(front.substr(1, i), middle.substr(0, i));
-            if(i != middle.size() - 1) //代表有右子樹
-                f(front.substr(i + 1, front.size() - 1 - i), middle.substr(i + 1, front.size() - i - 1));
-            cout << root;
-        }
-    }
+    int pos = middle.find(root);
 }
 int main()
 {
