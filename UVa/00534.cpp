018595a4950960c6c8606cd40c489a07a4f3d3e0
MUSEr60406 <sammy60406test@gmail.com>
2026-03-04 09:11:39 +0000

    2026年3月4日 下午5:11

--- UVa/00534.cpp
+++ UVa/00534.cpp
@@ -3,22 +3,16 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-
+int M[201][201];
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n ,count = 0;
-    int M[201][201];
     while(cin >> n && n != 0)
     {
-        vector<pii> stone;
-        for(int i = 0 ; i < n ; i++)
-        {
-            int x, y;
-            cin >> x >> y;
-            stone.push_back({x, y});
-        }
-    }
+        vector<pii> stone(n);
+        for(auto &i : stone)
+            cin >> i.first >> i.second;
 
     return 0;
 }
