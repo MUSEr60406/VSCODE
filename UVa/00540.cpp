eaa66bf5116f5ff7e10d8e43a4f1b830444b824a
MUSEr60406 <sammy60406test@gmail.com>
2026-03-05 03:12:53 +0000

    2026年3月5日 上午11:12

--- UVa/00540.cpp
+++ UVa/00540.cpp
@@ -39,7 +39,7 @@ int main()
                     if(index == -1)
                         q.push_back(number);
                     else
-                        q.insert(q.begin() + index + 1, number);
+                        q.insert(q.begin() + index, number);
                 }
                 else
                     q.push_back(number);
