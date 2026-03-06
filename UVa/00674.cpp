9d092d70e8dfb10da73d07d9db3e13f883bc4376
MUSEr60406 <sammy60406test@gmail.com>
2026-03-06 08:33:32 +0000

    2026年3月6日 下午4:33

--- UVa/00674.cpp
+++ UVa/00674.cpp
@@ -3,13 +3,11 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    ll dp[7490], coin[] = {1, 5, 10, 25, 50};
+    ll int dp[8000] = {0}, coin[] = {1, 5, 10, 25, 50};
     dp[0] = 1;
-    dp[1] = 1;
     for(ll &c : coin)
     {
         for(int i = c ; i <= 7489 ; i++)
@@ -18,6 +16,5 @@ int main()
     int n;
     while(cin >> n)
         cout << dp[n] << "\n";
-
     return 0;
 }
