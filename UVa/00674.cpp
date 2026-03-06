7d56bdc42fb04bed7e9e9a4231fc0ab3f5f1e570
MUSEr60406 <sammy60406test@gmail.com>
2026-03-06 08:24:16 +0000

    2026年3月6日 下午4:24

---
+++ UVa/00674.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int dp[7489];
+    dp[0] = 0;
+    dp[1] = 1;
+
+    return 0;
+}
