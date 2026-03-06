032587f1d84dd8374f7462d5eea7f3a6c4a08772
MUSEr60406 <sammy60406test@gmail.com>
2026-03-06 08:29:41 +0000

    2026年3月6日 下午4:29

--- UVa/00674.cpp
+++ UVa/00674.cpp
@@ -7,14 +7,17 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int dp[7489], coin[] = {1, 5, 10, 25, 50};
+    ll dp[7490], coin[] = {1, 5, 10, 25, 50};
     dp[0] = 0;
     dp[1] = 1;
-    for(int &c : coin)
+    for(ll &c : coin)
     {
         for(int i = c ; i <= 7489 ; i++)
             dp[i] += dp[i - c];
     }
+    int n;
+    while(cin >> n)
+        cout << dp[n] << "\n";
 
     return 0;
 }
