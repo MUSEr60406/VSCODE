cba7b57d11e4aac371fae174fed4da89ca8e2dff
MUSEr60406 <sammy60406test@gmail.com>
2026-03-07 07:36:26 +0000

    2026年3月7日 下午3:36

---
+++ UVa/00679.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int d;
+        cin >> d;
+        vector<int> node((1 - pow(2, d)) / 1 - 2);
+        cout << node.size();
+    }
+
+    return 0;
+}
