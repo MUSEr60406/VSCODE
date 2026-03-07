c6a4bded95ba04ec32ff3d604f463a8c6935c16c
MUSEr60406 <sammy60406test@gmail.com>
2026-03-07 09:53:24 +0000

    2026年3月7日 下午5:53

--- UVa/00722.cpp
+++ UVa/00722.cpp
@@ -39,9 +39,7 @@ int main()
         cin.ignore();
         while(getline(cin, s) && !s.empty()) 
             Map.push_back(s);
-        cout << bfs({x - 1, y -1}, Map);
-        if(n)
-            cout << "\n";
+        cout << bfs({x - 1, y -1}, Map) << "\n";
     }
     return 0;
 }
