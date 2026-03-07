54f5793c25c8e7ac0adc83a095b463b6bcc43c7b
MUSEr60406 <sammy60406test@gmail.com>
2026-03-07 08:44:58 +0000

    2026年3月7日 下午4:44

---
+++ UVa/00686.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    vector<bool> isprime(31701, true);
+    vector<int> prime;
+    for(int i = 2 ; i <= 31700 ; i++)
+    {
+        if(isprime[i] == true && i % 2 != 0)
+            prime.push_back(i);
+        for(int &j : prime)
+        {
+            if(i * j > 31700)
+                break;
+            isprime[i * j] = false;
+            if(i % j == 0)
+                break;
+        }
+    }
+
+    return 0;
+}
