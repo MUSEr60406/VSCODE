d868a1ee0181d1f00e4fa3298a53c83541f03cc9
MUSEr60406 <sammy60406test@gmail.com>
2026-03-07 08:58:16 +0000

    2026年3月7日 下午4:58

--- UVa/00686.cpp
+++ UVa/00686.cpp
@@ -7,40 +7,20 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    vector<bool> isprime(301, true);
+  vector<bool> isprime(33001, true);
     vector<int> prime;
-    for(int i = 2 ; i <= 300 ; i++)
+    for(int i = 2 ; i <= 33000 ; i++)
     {
         if(isprime[i] == true)
             prime.push_back(i);
         for(int &j : prime)
         {
-            if(i * j > 300)
+            if(i * j > 33000)
                 break;
             isprime[i * j] = false;
             if(i % j == 0)
                 break;
         }
-    }
-    int n;
-    for(int i : prime)
-        cout << i << "\n";
-    vector<bool> visited(prime.back() + 1, false);
-    while(cin >> n && n != 0)
-    {
-        int ans = 0;
-        for(int i = 0 ; i < prime.size() - 1 ; i++)
-        {
-            for(int j = i + 1 ; j < prime.size() ; j++)
-            {
-                if(!visited[i] && !visited[j] && prime[i] + prime[j] == n)
-                {
-                    visited[i] = visited[j] = true;
-                    ans++;
-                }
-            }
-        }
-        cout << ans << "\n";
     }
     return 0;
 }
