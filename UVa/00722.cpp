74b58cb06d3a98bdbe0403f97df32ed41e2a2372
MUSEr60406 <sammy60406test@gmail.com>
2026-03-07 09:28:21 +0000

    2026年3月7日 下午5:28

---
+++ UVa/00722.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        cin.ignore();
+        pii p;
+        vector<string> Map;
+        string s;
+        cin >> p.first >> p.second;
+        while(getline(cin, s))
+            Map.push_back(s);
+
+    }
+    return 0;
+}
