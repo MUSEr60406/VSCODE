1e8ccb91fec866898f2a5af6a14b5ecffd707a31
MUSEr60406 <sammy60406test@gmail.com>
2026-03-08 06:57:22 +0000

    2026年3月8日 下午2:57

--- UVa/00748.cpp
+++ UVa/00748.cpp
@@ -7,23 +7,24 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    string s;
-    while(getline(cin, s) && !s.empty())
+    string n;
+    int power;
+    while(cin >> n >> power)
     {
         int decimal = 0;
-        stringstream ss(s);
-        string number, power;
-        while(ss >> number >> power)
-        vector<int> num(100, 0);
-        for(int i = 0 ; i < number.size() ; i++)
+        vector<int> number(100, 0);
+        for(int i = 0 ; i < n.size() ; i++)
         {
-            if(s[i] == '.')
+            if(n[i] == '.')
             {
-                decimal = number.size() - i;
-                break;
+                decimal = n.size() - i;
+                continue;
             }
+            number[i] = n[i] - '0';
         }
-        
+        for(int &i : number)
+            cout << i << " ";
+        cout << "\n" << decimal;        
     }
 
     return 0;
