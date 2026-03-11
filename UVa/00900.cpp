a136a17a17e5edca2eebb3c83eae6c10b4117a77
MUSEr60406 <sammy60406test@gmail.com>
2026-03-11 11:58:35 +0000

    2026年3月11日 下午7:58

--- UVa/00900.cpp
+++ UVa/00900.cpp
@@ -12,10 +12,11 @@ int main()
     dp[0] = 0;
     dp[1] = 1;
     dp[2] = 2;
-    while(cin >> n && n != 0)
-    {
+    for(int i = 3 ; i <= 50 ; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
 
-    }
+    while(cin >> n && n != 0)
+        cout << dp[n] << "\n";
 
     return 0;
 }
