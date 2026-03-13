53dfe351137343b4cb2ce78ff40b55c49f80db83
MUSEr60406 <sammy60406test@gmail.com>
2026-03-13 12:41:49 +0000

    2026年3月13日 下午8:41

---
+++ UVa/10041.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int s, r[500];
+        cin >> s;
+        for(int i = 0 ; i < s ; i++)
+            cin >> r[i];
+    }
+
+    return 0;
+}
