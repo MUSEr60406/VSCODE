dc9409f6b39ea8426e819889596c58cbdbd0f7fe
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 15:13:26 +0000

    2026年3月15日 下午11:13

--- UVa/10041.cpp
+++ UVa/10041.cpp
@@ -16,13 +16,14 @@ int main()
         cin >> r;
         for(int i = 0 ; i < r ; i++)
             cin >> s[i];
-        int *n = &s;
+        
+        cout << *(s + 1) << "\n";
         sort(s, s + n);
         int mid = (r - 1) / 2, total_dis = 0; //r - 1是因為index是0 ~ r - 1
-        for(int i = 0 ; i < r ; i++)
-            total_dis += abs(s[mid] - s[i]); //abs -> 絕對值
+        // for(int i = 0 ; i < r ; i++)
+        //     total_dis += abs(s[mid] - s[i]); //abs -> 絕對值
 
-        cout << total_dis << "\n";
+        // cout << total_dis << "\n";
     }
 
     return 0;
