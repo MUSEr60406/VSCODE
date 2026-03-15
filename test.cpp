68281a201d1a3d671d18d60e6de2f1185108b053
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 15:15:34 +0000

    2026年3月15日 下午11:15

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+#define pb push_back
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    vector<int> s(500, 0);
+    sort(s.begin(), s.end());
+
+    return 0;
+}
