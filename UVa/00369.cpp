0bb4472d69d257d161b8b723b7f35af4361283bf
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 12:47:22 +0000

    2026年3月15日 下午8:47

--- UVa/00369.cpp
+++ UVa/00369.cpp
@@ -4,7 +4,7 @@
 #define ll long long
 #define pb push_back
 using namespace std;
-unsigned ll c[101][101];
+ll c[101][101];
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -22,7 +22,9 @@ int main()
                 c[i][j]  =c[i - 1][j - 1] + c[i - 1][j - 1];
         }
     }
-    int m, n;
+    int n, m;
+    while(cin >> n >> m && n != 0 && m != 0)
+        cout << n << " things taken " << m << " at a time is " << c[n][m] << " exactly.\n";
 
     return 0;
 }
