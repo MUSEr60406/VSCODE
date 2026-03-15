025304a4b9b86ae06cdfd80cc0e505ee92adc999
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 13:10:44 +0000

    2026年3月15日 下午9:10

--- UVa/10327.cpp
+++ UVa/10327.cpp
@@ -18,11 +18,11 @@ int main()
             cin >> v[i];
         for(int i = 0 ; i < n - 1 ; i++)
         {
-            for(int j = i ; j < n - i - 1 ; j++)
+            for(int j = 0 ; j < n - i - 1 ; j++)
             {
                 if(v[j] > v[j + 1])
                 {
-                    swap(v[j + 1], v[j]);
+                    swap(v[j], v[j + 1]);
                     cnt++;
                 }
             }
