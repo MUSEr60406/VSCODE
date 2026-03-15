d01feec37f881ccef5f4f3dbb172722443e12f96
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 15:10:03 +0000

    2026年3月15日 下午11:10

--- UVa/10041.cpp
+++ UVa/10041.cpp
@@ -16,17 +16,8 @@ int main()
         cin >> r;
         for(int i = 0 ; i < r ; i++)
             cin >> s[i];
-
-        for(int i = 0 ; i < r ; i++) //r - 1
-        {
-            for(int j = i + 1 ; j < r ; j++)
-            {
-                if(s[j] < s[i])
-                    swap(s[i], s[j]);
-            }
-        }
-        vector<int> v;
-
+        int *n = &s;
+        sort(s, s + n);
         int mid = (r - 1) / 2, total_dis = 0; //r - 1是因為index是0 ~ r - 1
         for(int i = 0 ; i < r ; i++)
             total_dis += abs(s[mid] - s[i]); //abs -> 絕對值
