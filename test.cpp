4bd1d008fe8b137e67958b90bdee4cfdde5eea50
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 15:17:25 +0000

    2026年3月15日 下午11:17

--- test.cpp
+++ test.cpp
@@ -8,8 +8,10 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    vector<int> s(500, 0);
-    sort(s.begin(), s.end());
-
+    vector<int> s; //{}
+    s.push_back(2);
+    s.push_back(1); //{2, 1}
+    s.pop_back(); //{2}
+    s.assign(10, 2);
     return 0;
 }
