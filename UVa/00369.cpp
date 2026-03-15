b9370c7a317017631ffc933072f6dbd08cae1cdb
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 12:42:05 +0000

    2026年3月15日 下午8:42

---
+++ UVa/00369.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+#define pb push_back
+using namespace std;
+ll c[101][101];
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    for(int i = 0 ; i <= 100 ; i++)
+    {
+        for(int j = 0 ; j <= 100 ; j++)
+        {
+            if(j == 1)
+                c[i][j] = i;
+            else if(i == j)
+                c[i][j] = 1;
+            else if(i < j)
+                c[i][j] = 0;
+            else
+                c[i][j]  =c[i - 1][j - 1] + c[i - 1][j - 1];
+        }
+    }
+
+    return 0;
+}
