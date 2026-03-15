ac13d7c85bc5c9e585e0b8e6782ad4f853cf5559
MUSEr60406 <sammy60406test@gmail.com>
2026-03-15 15:47:59 +0000

    2026年3月15日 下午11:47

--- UVa/00118.cpp
+++ UVa/00118.cpp
@@ -9,12 +9,14 @@ int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int r, c, x, y;
-    while(cin >> r >> c >> x >> y)
+    char D;
+    string command;
+    while(cin >> r >> c >> x >> y >> D >> command)
     {
-        bool map[51][51];
+        bool map[51][51] = {false};
         char move[4] = {'N', 'S', 'E', 'W'};    
         int dx[4] = {0, 0, 1, -1} , dy[4] = {1, -1, 0, 0};
-
+    }
 
     return 0;
 }
