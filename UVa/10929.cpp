d44560ce5ae4a307cc567fa1f39a5ecac64a9533
MUSEr60406 <sammy60406test@gmail.com>
2026-03-16 17:09:58 +0000

    2026年3月17日 上午1:09

--- UVa/10929.cpp
+++ UVa/10929.cpp
@@ -13,17 +13,16 @@ int main()
         int diff, odd = 0, even = 0;
         for(int i = 0 ; i < s.size() ; i++)
         {
-            if(i % 2 == 0)
-                even += s[i] - '0';
+            if((i + 1) % 2 == 0)
+                even += (s[i] - '0');
             else
-                odd += s[i] - '0';
+                odd += (s[i] - '0');
         }
         diff = abs(even - odd);
-        if(diff == 0 || diff == 11)
+        if(diff % 11 == 0)
             cout << s << " is a multiple of 11.\n";
         else
             cout << s << " is not a multiple of 11.\n";
     }
-       
     return 0;
 }
