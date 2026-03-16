0d33310766e91121acf7d8b7e2926af989af3e91
MUSEr60406 <sammy60406test@gmail.com>
2026-03-16 15:37:34 +0000

    2026年3月16日 下午11:37

--- UVa/00315.cpp
+++ UVa/00315.cpp
@@ -8,29 +8,17 @@ vector<vector<int>> g;
 vector<int> ids;
 vector<int> low;
 vector<bool> cut;
-void tarjan(int start)
+void dfs(int u, int p)
 {
-    ids[start] = low[start] = t++;
+    ids[u] = low[u] = t++;
+    int children = 0;
     
-    for(int &v : g[start])
+    for(int &v : g[u])
     {
-        if(ids[v] == 0)
-            tarjan(v);
-        else if(instack[v])
-            low[start] = min(low[start], low[v]);
+        
     }
-    if(ids[start] == low[start])
-    {
-        scc++;
-        while(true)
-        {
-            int node = st.top();
-            st.pop();
-            instack[node] = false;
-            if(start == node)
-                break;
-        }
-    }
+    
+    
 }
 int main()
 {
@@ -57,10 +45,8 @@ int main()
         low.assign(n + 1, 0); //low-link
         cut.assign(n + 1, false);
         for(int i = 1 ; i <= n ; i++)
-        {
             if(ids[i] == 0)
-                tarjan(i);
-        }
+                dfs(i, -1);
     }
 
     return 0;
