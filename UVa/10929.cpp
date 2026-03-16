9228b5e61ea2f14207d560c717e23751378faa13
MUSEr60406 <sammy60406test@gmail.com>
2026-03-16 16:41:08 +0000

    2026年3月17日 上午12:41

---
+++ UVa/10929.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    while(cin >> n && n != 0)
+        cout << n << " is " << (n % 11 == 0 ? "" : "not " ) << "a multiple of 11.\n";
+    return 0;
+}
