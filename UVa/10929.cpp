7978a139d085e8e9ad1de90fcd70e77270399f28
MUSEr60406 <sammy60406test@gmail.com>
2026-03-16 16:47:27 +0000

    2026年3月17日 上午12:47

--- UVa/10929.cpp
+++ UVa/10929.cpp
@@ -7,8 +7,23 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n;
-    while(cin >> n && n != 0)
-        cout << n << " is " << (n % 11 == 0 ? "" : "not ") << "a multiple of 11.\n";
+    string s;
+    while(cin >> s && s != "0")
+    {
+        int diff, odd = 0, even = 0;
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if(i % 2 == 0)
+                even += s[i] - '0';
+            else
+                odd += s[i] - '0';
+        }
+        diff = abs(even - odd);
+        if(diff == 0 || diff == 11)
+            cout << s << " is a multiple of 11.\n";
+        else
+            cout << s << " is not a multiple of 11.\n";
+    }
+       
     return 0;
 }
