7497ea33f4f6c3165ae7eb69bc7cc2295dc789a8
MUSEr60406 <sammy60406test@gmail.com>
2026-03-17 15:56:22 +0000

    2026年3月17日 下午11:56

---
+++ UVa/10420.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    string s; 
+    while(getline(cin, s))
+    {
+        string country = "";
+        map<string,int> country_cnt;
+    }
+    return 0;
+}
