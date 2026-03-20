f93b48a6a8c505e0e6a8c24a3aa4a3e774113c1a
MUSEr60406 <sammy60406test@gmail.com>
2026-03-20 16:26:26 +0000

    2026年3月21日 上午12:26

--- test.cpp
+++ test.cpp
@@ -1,37 +1,41 @@
 #include<iostream>
+#include <algorithm>
 #include<vector>
-#include<algorithm>
 using namespace std;
 
 int main(){
-    int n,m,nm, counts ;
+    int n, m ,nm ,nm1, a;
     while(cin >> n)
     {
-        //int  s[1000000];
-        vector<int> s(n);
-        for(int i = 0; i < n; i++ )
+        int counts = 0;
+        vector <int> s(n);
+        for(int i = 0; i < n; i++)
         {
             cin >> s[i];
         }
-        sort(s.begin(), s.end()); //0 1 2 3 4    0 begin, 5 end
+        sort(s.begin(),s.end());
         if (n % 2 == 0 )
         {
-             m = n / 2;
-             nm = s[m-1];
+             m = (n - 1) / 2; 
+             nm = s[m];
+             nm1 = s[m + 1];
+             a = s[m + 1] - s[m] + 1;   
         }
         else
         {
-            m = (n + 1) / 2;
-            nm = s[m-1];
+            m = (n - 1) / 2;
+            nm = s[m];
+            a = 1;
         }
-        for (int i = 0; i <= m - 1; i++)
+        for (int i = 0; i < n; i++)
         {
             if (s[i] == nm) counts++;
+
         }
-        cout << nm << " " << counts;
+        cout << nm << " " << counts << " " << a << "\n";
 
     }
 
-
+return 0;
 
 }
