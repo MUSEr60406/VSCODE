0e4f4715c63db5b29f7f6e79f2ea5e9d756bd39a
MUSEr60406 <sammy60406test@gmail.com>
2026-03-20 15:46:16 +0000

    2026年3月20日 下午11:46

--- test.cpp
+++ test.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main(){
     int n,m,nm, counts ;
     while(cin >> n)
     {
-        int  s[1000000];
+        //int  s[1000000];
+        vector<int> s(n);
         for(int i = 0; i < n; i++ )
         {
             cin >> s[i];
         }
-        sort(s, s + n);
+        sort(s, s + n); //0 1 2 3 4    0 begin, 5 end
         if (n %2 == 0 )
         {
              m = n / 2;
