590a766136f45d14ae0da6ef57b02655da6ec1e5
MUSEr60406 <sammy60406test@gmail.com>
2026-03-21 07:39:14 +0000

    2026年3月21日 下午3:39

--- UVa/11332.cpp
+++ UVa/11332.cpp
@@ -5,20 +5,22 @@
 using namespace std;
 string f(string s)
 {
-    if(s.size() == 1)
-        return s;
     int n = 0;
+    string temp = "";
     for(auto &c : s)
         n += c - '0';
+    temp = to_string(n);
+    if(temp.size() == 1)
+        return temp;
+    else
+        return f(temp);
 }
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     string s;
     while(cin >> s && s != "0")
-    {
-        cout << f(s);
-    }
+        cout << f(s) << "\n";
 
     return 0;
 }
