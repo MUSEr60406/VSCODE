00df49bed1e6d8a45ffab45e9df7486c5db11ef9
MUSEr60406 <sammy60406test@gmail.com>
2026-03-26 12:48:25 +0000

    2026年3月26日 下午8:48

--- UVa/12797.cpp
+++ UVa/12797.cpp
@@ -6,13 +6,14 @@ using namespace std;
 int N;
 vector<string> g;
 vector<vector<int>> dis;
-unordered_map<int, char> um;
+vector<char> c;
 void bfs()
 {
     int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
     queue<pii> q;
     q.push({0, 0});
     dis[0][0] = 0;
+    c[tolower(g[0][0] - 'a')] = g[0][0];
     while(!q.empty())
     {
         auto [x, y] = q.front();
@@ -20,9 +21,9 @@ void bfs()
         for(int i = 0 ; i < 4 ; i++)
         {
             int newx = x + dx[i], newy = y + dy[i];
-            if(newx < 0 || newx >= N || newy < 0 || newy >= N || dis[newx][newy] != -1 || um[tolower(g[newx][newy]) - 'a'] != g[newx][newy])
+            if(newx < 0 || newx >= N || newy < 0 || newy >= N || dis[newx][newy] != -1 || c[tolower(g[newx][newy]) - 'a'] != g[newx][newy])
                 continue;
-            um[tolower(g[newx][newy] - 'a')] = g[newx][newy];
+            c[tolower(g[newx][newy]) - 'a'] = g[newx][newy];
             dis[newx][newy] = dis[x][y] + 1;
             q.push({newx, newy});
         }
@@ -34,7 +35,8 @@ int main()
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     while(cin >> N)
     {
-        um.clear();
+        c.clear();
+        c.resize(10);
         g.clear();
         g.resize(N);
         dis.assign(N, vector<int>(N, -1));
