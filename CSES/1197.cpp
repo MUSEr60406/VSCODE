36b969c28b92af554aa98d1877b797ad7e6689e6
MUSEr60406 <sammy60406test@gmail.com>
2026-03-26 11:10:43 +0000

    2026年3月26日 下午7:10

--- CSES/1197.cpp
+++ CSES/1197.cpp
@@ -12,7 +12,9 @@ int main()
     cin >> n >> m;
     vector<vector<pii>> MAP(n + 1);
     vector<int> dis(n + 1, INF);
-    for(int i = 0 ; i < n ; i++)
+    vector<int> Cycle;
+    vector<bool> visited(n + 1, false);
+    for(int i = 0 ; i < m ; i++)
     {
         int a, b, w;
         cin >> a >> b >> w;
@@ -25,9 +27,40 @@ int main()
         {
             for(auto &[v, w] : MAP[i])
             {
-                if(dis[i] + w < dis[v])
-                dis[v] = dis[i] + w;        
+                if(dis[i] + w < dis[v] && dis[i] != INF)
+                    dis[v] = dis[i] + w;        
             }
+        }
+    }
+    for(int k = 0 ; k < n - 1 ; k++)
+    {
+        for(int i = 1 ; i <= n ; i++)
+        {
+            for(auto &[v, w] : MAP[i])
+            {
+                if(dis[i] + w < dis[v] && dis[i] != INF)
+                {
+                    if(visited[i] == false)
+                    {
+                        Cycle.push_back(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+        }
+    }
+    if(Cycle.empty())
+        cout << "NO\n";
+    else
+    {
+        cout << Cycle.size() << "\n";
+        int end = Cycle[0];
+        cout << "YES\n";
+        for(int i = 0 ; i < Cycle.size(); i++)
+        {
+            if(i > 0 && Cycle[i] == end)
+                break;
+            cout << Cycle[i] << " ";
         }
     }
     return 0;
