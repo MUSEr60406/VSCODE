7fe4fcf37b2ab902770860df5811920a8899f770
MUSEr60406 <sammy60406test@gmail.com>
2026-03-26 12:33:53 +0000

    2026年3月26日 下午8:33

--- UVa/12797.cpp
+++ UVa/12797.cpp
@@ -3,27 +3,40 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
+int N;
 vector<string> g;
+vector<vector<int>> dis;
 void bfs()
 {
     int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+    unordered_map<char, bool> um;
     queue<pii> q;
     q.push({0, 0});
+    dis[0][0] = 0;
     while(!q.empty())
     {
         auto [x, y] = q.front();
         q.pop();
-        
+        for(int i = 0 ; i < 4 ; i++)
+        {
+            int newx = x + dx[i], newy = y + dy[i];
+            if(newx < 0 || newx >= N || newy < 0 || newy >= N || dis[newx][newy] != -1 || um[tolower(g[newx][newy])])
+                continue;
+            dis[newx][newy] = dis[x][y] + 1;
+            um[tolower(g[newx][newy])] = true;
+            q.push({newx, newy});
+        }
     }
+    cout << dis[N - 1][N - 1] << "\n";
 }
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int N;
     while(cin >> N)
     {
         g.clear();
         g.resize(N);
+        dis.assign(N, vector<int>(N, -1));
         for(auto &s : g)
             cin >> s;
         bfs();
