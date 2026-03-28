95cd6a74bf3f943bfa9c097c27a98686433c824a
MUSEr60406 <sammy60406test@gmail.com>
2026-03-28 10:38:32 +0000

    2026年3月28日 下午6:38

---
+++ UVa/10004.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+vector<vector<int>> g;
+vector<bool> color;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    while(cin >> n && n != 0)
+    {
+        
+    }
+    
+
+    return 0;
+}
