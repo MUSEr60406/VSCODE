04979b124f2d4d3df1fde69981410dd7ed213a15
MUSEr60406 <sammy60406test@gmail.com>
2026-03-28 10:47:04 +0000

    2026年3月28日 下午6:47

--- UVa/10004.cpp
+++ UVa/10004.cpp
@@ -4,14 +4,50 @@
 #define ll long long
 using namespace std;
 vector<vector<int>> g;
-vector<bool> color;
+vector<int> color;
+bool bfs()
+{
+    queue<int> q;
+    q.push(0);
+    bool c = 1;
+    color[0] = c;
+    while(!q.empty())
+    {
+        int now = q.front();
+        c = color[now];
+        q.pop();
+        for(int &v : g[now])
+        {
+            if(color[v] == -1)
+            {
+                color[v] = !c;
+                q.push(v); 
+            }
+            else if(color[v] == c)
+                return false;
+        }
+    }
+    return true;
+}
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n;
     while(cin >> n && n != 0)
     {
-        
+        g.clear();
+        g.resize(n);
+        color.assign(n, -1);
+        int m;
+        cin >> m;
+        for(int i = 0 ; i < m ; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+            g[a].push_back(b);
+            g[b].push_back(a);
+        }
+        cout << (bfs() ? "BICOLORABLE." : "NOT BICOLORABLE.") << "\n";
     }
     
 
