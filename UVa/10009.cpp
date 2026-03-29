8c4d92bac409e94003b0eb9f5e40f9c7037388d0
MUSEr60406 <sammy60406test@gmail.com>
2026-03-29 03:03:20 +0000

    2026年3月29日 上午11:03

--- UVa/10009.cpp
+++ UVa/10009.cpp
@@ -6,7 +6,13 @@ using namespace std;
 vector<vector<int>> tree;
 vector<int> path;
 vector<int> parent;
-vector<pii> test_case;
+void bfs(int st, int en)
+{
+    queue<int> q;
+    q.push(st);
+    path[st] = 0;
+
+}
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -19,14 +25,20 @@ int main()
         cin >> m >> n;
         tree.clear();
         tree.resize(m);
-        path.assign(26, -1);
-        parent.assign(26, -1);
-        test_case.clear();
         for(int i = 0 ; i < m ; i++)
         {
             string c1, c2;
             cin >> c1 >> c2;
-            
+            tree[c1[0] - 'A'].push_back(c2[0] - 'A');
+            tree[c2[0] - 'A'].push_back(c1[0] - 'A');
+        }
+        for(int i = 0 ; i < n ; i++)
+        {
+            path.assign(26, -1);
+            parent.assign(26, -1);
+            string st, en;
+            cin >> st >> en;
+            bfs(st[0] - 'A', en[0] - 'A');
         }
     }
 
