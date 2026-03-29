6ebb37d05216706b985b3e3bcba2d87501cd5b8b
MUSEr60406 <sammy60406test@gmail.com>
2026-03-29 02:47:05 +0000

    2026年3月29日 上午10:47

---
+++ UVa/10009.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+vector<int> par;
+vector<int> path;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t;
+    cin.ignore();
+    cin >> t;
+    while(t--)
+    {
+        int m, n;
+        cin >> m >> n;
+        par.assign(26, -1);
+        path.assign(26, -1);
+    }
+
+    return 0;
+}
