b32cdc2c083556e7cc80b87dcd6802bc4666daaf
MUSEr60406 <sammy60406test@gmail.com>
2026-03-30 15:24:02 +0000

    2026年3月30日 下午11:24

--- UVa/01208.cpp
+++ UVa/01208.cpp
@@ -3,7 +3,21 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-
+int read()
+{
+    int num = 0;
+    char c  = getchar();
+    while(c != EOF && (c < '0' || c > '9'))
+        c = getchar();
+    while(c == EOF)
+        return -1;
+    while(c >= '0' && c <= '9')
+    {
+        num = num * 10 + (c - '0');
+        c = getchar();
+    }
+    return num;
+}
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -11,8 +25,15 @@ int main()
     cin >> t;
     while(t--)
     {
-        memset(g, 0. sizeof(g));
+        memset(g, 0, sizeof(g));
         cin >> n;
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = 0 ; j < n ; j++)
+            {
+                g[i][j] = read();
+            }
+        }
     }
 
     return 0;
