3d9505faef8d823e49caa83727058b1ff2c0df71
MUSEr60406 <sammy60406test@gmail.com>
2026-04-01 11:50:11 +0000

    2026年4月1日 下午7:50

--- test/hw.cpp
+++ test/hw.cpp
@@ -1,13 +1,63 @@
-#include<bits/stdc++.h>
-#define pii pair<int,int>
-#define pll pair<long,long>
-#define ll long long
+#include <iostream>
 using namespace std;
+
+class Complex
+{
+private:
+    double real;
+    double imaginary;
+
+public:
+    Complex(double r = 0, double i = 0)
+        : real(r), imaginary(i)
+    {
+    }
+
+    Complex operator*(const Complex &other) const
+    {
+        return
+        {
+            real * other.real - imaginary * other.imaginary,
+            real * other.imaginary + imaginary + other.real
+        };
+    }
+
+    bool operator==(const Complex &other) const
+    {
+        return (real == other.real && imaginary == other.imaginary);
+    }
+
+    bool operator!=(const Complex &other) const
+    {
+        return *this != other;
+    }
+
+    friend istream &operator>>(istream &in, Complex &c)
+    {
+        in >> c.real >> c.imaginary;
+        return in;
+    }
+
+   friend ostream &operator<<(ostream &out, const Complex &c)
+   {
+        out << c.real;
+        double i = abs(c.imaginary);
+        cout << (c.imaginary >= 0 ? " + " : " - ") << i << "i";
+        return out;
+    }
+}
 
 int main()
 {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    
+    Complex c1, c2;
+
+    cin >> c1 >> c2;
+
+    cout << "Complex1: " << c1 << endl;
+    cout << "Complex2: " << c2 << endl;
+    cout << "Product: " << (c1 * c2) << endl;
+    cout << "Equal: " << (c1 == c2 ? "True" : "False") << endl;
+    cout << "Not Equal: " << (c1 != c2 ? "True" : "False") << endl;
 
     return 0;
 }
