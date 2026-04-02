5fd066470aa850cf34482ff6230819db8dc6399e
MUSEr60406 <sammy60406test@gmail.com>
2026-04-02 05:21:49 +0000

    2026年4月2日 下午1:21

--- CSES/1195.cpp
+++ CSES/1195.cpp
@@ -5,37 +5,44 @@
 using namespace std;
 int n, m;
 vector<vector<pii>> flight;
-vector<int> cost;
+ll cost[100005][2];
 void dijkstra()
 {
-    ll max = -1;
-    priority_queue<pll, vector<pll>, greater<pll>> pq;
-    pq.push({0, 1});
+    for(int i = 1 ; i <= n ; i++)
+        cost[i][0] = cost[i][1] = LLONG_MAX / 2;
+    priority_queue<tuple<ll, ll, ll>, vector<tuple<ll, ll, ll>>, greater<tuple<ll, ll, ll>>> pq;
+    cost[1][0] = 0;
+    pq.push({0, 1, 0});
     while(!pq.empty())
     {
-        auto [d, node] = pq.top();
+        auto [d, node, coupon] = pq.top();
         pq.pop();
-        if(d > cost[node])
+        if(d > cost[node][coupon])
             continue;
         for(auto &[v, w] : flight[node])
         {
-            if(cost[v] > d + w)
+            if(cost[v][coupon] > d + w)
             {
-                if(w > max)
-                    max = w;
-                cost[v] = d + w;
-                pq.push({cost[v], v});
+                cost[v][coupon] = d + w;
+                pq.push({cost[v][coupon], v, coupon});
+            }
+            if(coupon == 0)
+            {
+                if(cost[v][1] > d + w / 2)
+                {
+                    cost[v][1] = d + w / 2;
+                    pq.push({cost[v][1], v, 1});
+                }
             }
         }
     }
-    cout << cost[n] - max / 2 << "\n";
+    cout << cost[n][1] << "\n";
 }
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> n >> m;
     flight.resize(n + 1);
-    cost.assign(n + 1, LLONG_MAX / 2);
     for(int i = 0 ; i < m ; i++)
     {
         int a, b, c;
