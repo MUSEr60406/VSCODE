357b5b5890c4cd369fb8a95b48cf6c6db65e077d
MUSEr60406 <sammy60406test@gmail.com>
2026-04-02 05:10:14 +0000

    2026年4月2日 下午1:10

--- CSES/1195.cpp
+++ CSES/1195.cpp
@@ -3,21 +3,45 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
+int n, m;
 vector<vector<pii>> flight;
 vector<int> cost;
+void dijkstra()
+{
+    ll max = -1;
+    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    pq.push({0, 1});
+    while(!pq.empty())
+    {
+        auto [d, node] = pq.top();
+        pq.pop();
+        if(d > cost[node])
+            continue;
+        for(auto &[v, w] : flight[node])
+        {
+            if(cost[v] > d + w)
+            {
+                if(w > max)
+                    max = w;
+                cost[v] = d + w;
+                pq.push({cost[v], v});
+            }
+        }
+    }
+}
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, m;
     cin >> n >> m;
     flight.resize(n + 1);
-    cost.assign(n + 1, -1);
+    cost.assign(n + 1, LLONG_MAX / 2);
     for(int i = 0 ; i < m ; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
         flight[a].push_back({b, c});
     }
+    dijkstra();
 
     return 0;
 }
