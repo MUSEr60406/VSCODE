62942e4cb6ce4aa32c7b0f1d5674ef7e68e13ef6
MUSEr60406 <sammy60406test@gmail.com>
2026-04-06 11:36:16 +0000

    2026年4月6日 下午7:36

---
+++ UVa/00423.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, INF = INT_MAX / 2;
+    while(cin >> n)
+    {
+        int adj[101][101];
+        memset(adj, INF, sizeof(adj));
+        for(int i = 1 ; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= i ; j++)
+            {
+                string s;
+                cin >> s;
+                adj[i][j] = (s != "x" ? stoi(s) : INF);
+            }
+        }
+        //
+        for(int k = 1 ; k <= n ; k++)
+            for(int i = 1 ; i <= n ; i++)
+                for(int j = 1 ; j <= n ; j++)
+                    adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+        int max = -1;
+        for(int i = 2 ; i <= n ; i++)
+            if(adj[1][i] > max) 
+                max = adj[1][i];
+        cout << max << "\n";
+    }
+
+    return 0;
+}
