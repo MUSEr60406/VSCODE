6ba7c98173f33d9d69db2c9f8d758d7982735544
MUSEr60406 <sammy60406test@gmail.com>
2026-04-06 11:45:01 +0000

    2026年4月6日 下午7:45

--- UVa/00423.cpp
+++ UVa/00423.cpp
@@ -12,8 +12,10 @@ int main()
         int adj[105][105];
         memset(adj, INF, sizeof(adj));
         for(int i = 1 ; i <= n ; i++)
+            adj[i][i] = 0;
+        for(int i = 2 ; i <= n ; i++)
         {
-            for(int j = 1 ; j <= i ; j++)
+            for(int j = 1 ; j <= i - 1 ; j++)
             {
                 string s;
                 cin >> s;
