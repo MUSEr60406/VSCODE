72065e751718b3d684efc9d08eaa35773d84b3e6
MUSEr60406 <sammy60406test@gmail.com>
2026-04-08 12:11:17 +0000

    2026年4月8日 下午8:11

--- t.cpp
+++ t.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
+
 class c
 {
     private:
-        static int s;
+        static int s; // 宣告
     public:
         c (int i = 0)
         {
@@ -13,16 +14,22 @@ class c
         {
             s = a;
         }
+        // 修正：使用 out 而非 cout
         friend ostream &operator<<(ostream &out, c &C)
         {
             cout << C.s << "\n";
             return out;
         }
 };
+
+// 修正：必須在類別外定義並分配記憶體
+int c::s = 0; 
+
 int main()
 {
-    c A(1), B;
-    cout << A << B;
-    A.f(5);
-    cout << B;
+    c A(1), B;     // A 讓 s=1, B 接著讓 s=0
+    cout << A << B; // 都會輸出 0
+    A.f(5);        // 透過 A 修改 s 為 5
+    cout << B;     // B 看到的 s 也變成了 5
+    return 0;
 }
