88e6a7406c59e98730a17105ab5c42a6bb8bc816
MUSEr60406 <sammy60406test@gmail.com>
2026-04-18 06:57:33 +0000

    2026年4月18日 下午2:57

--- test.cpp
+++ test.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int main()
 {
-    cout << "hihihihihih646646467676767676767";
+    int n;
+    cout << "Hello World\n";
+    for(int i = 0 ; i < 5 ; i++)
+        cout << "676767676767";
     return 0;
 }
