99b71839e0f201cf32eddde9a7d00c6d7bc26197
MUSEr60406 <sammy60406test@gmail.com>
2026-04-20 12:23:15 +0000

    2026年4月20日 下午8:23

---
+++ UVa/00558.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+int n, m, INF = INT_MAX / 2;
+vector<vector<pii>> g;
+vector<int> d;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cin >> n;
+    while(n--)
+    {
+        cin >> m;
+        g.clear();
+        g.resize(m);
+        d.assign(m, INF);
+        for(int i = 0 ; i < m ; i++)
+        {
+            int u, v, w;
+            cin >> u >> v >> w;
+            g[u].push_back({v, w});
+        }
+    }
+
+    return 0;
+}
