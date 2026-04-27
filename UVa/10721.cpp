2b09c959d9f3d9ab4e984c8ea4c0fc044c75b27b
MUSEr60406 <sammy60406test@gmail.com>
2026-04-27 14:41:18 +0000

    2026年4月27日 下午10:41

--- UVa/10721.cpp
+++ UVa/10721.cpp
@@ -7,7 +7,10 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    
-
+    int n, m, k;
+    while(cin >> n >> m >> k)
+    {
+        vector<vector<ll>> dp(k + 1, vector<ll>(n + 1, 0));
+    }
     return 0;
 }
