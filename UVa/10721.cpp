bfd8f3ac1043c305d45134e28712678512fd874e
MUSEr60406 <sammy60406test@gmail.com>
2026-04-27 14:49:45 +0000

    2026年4月27日 下午10:49

--- UVa/10721.cpp
+++ UVa/10721.cpp
@@ -11,6 +11,16 @@ int main()
     while(cin >> n >> m >> k)
     {
         vector<vector<ll>> dp(k + 1, vector<ll>(n + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1 ; i <= k ; i++)
+        {
+            for (int j = 1 ; j <= n ; j++) 
+            { 
+                for (int w = 1 ; w <= m && w <= j ; w++) 
+                    dp[i][j] += dp[i - 1][j - w];
+            }
+        }
+        cout << dp[k][n] << "\n";
     }
     return 0;
 }
