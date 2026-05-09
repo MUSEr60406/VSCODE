2bb69298321c9decce893f5796463bc0603a0e9b
MUSEr60406 <sammy60406test@gmail.com>
2026-05-09 08:08:25 +0000

    2026年5月9日 下午4:08

--- CSES/1681.cpp
+++ CSES/1681.cpp
@@ -7,16 +7,16 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, m; 
+    int n, m;
     cin >> n >> m;
-    vector<vector<bool>> g(n + 1, vector<bool>(n + 1, false));
-    vector<ll> dis(n + 1, 0);
+    vector<vector<int>> g(n);
+    vector<int> deg(n + 1, 0);
     for(int i = 0 ; i < m ; i++)
     {
         int a, b;
         cin >> a >> b;
-        g[a][b] = 0;
+        g[a].push_back(b);
+        deg[b]++;
     }
-
     return 0;
 }
