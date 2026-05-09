8c2c369bf3cc29c896019bb1a681a63dbe67c3f3
MUSEr60406 <sammy60406test@gmail.com>
2026-05-09 06:30:15 +0000

    2026年5月9日 下午2:30

---
+++ UVa/10432.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+#define pi 3.14159265358979323846
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    double r, n;
+    while(cin >> r >> n)
+    {
+        double angle = 360 / n, a;
+        a = 1/2 * r * r * sin(angle) * n;
+        cout << fixed << setprecision(3) << a << "\n";
+    }
+
+    return 0;
+}
