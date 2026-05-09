7266e4c9cae9d0a5677482f4d4556954302e271b
MUSEr60406 <sammy60406test@gmail.com>
2026-05-09 08:11:56 +0000

    2026年5月9日 下午4:11

--- CSES/1681.cpp
+++ CSES/1681.cpp
@@ -3,14 +3,34 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-
+int n, m;
+vector<vector<int>> g(n);
+vector<int> deg(n + 1, 0);
+vector<int> dp(n + 1, 0);
+void bfs()
+{
+    queue<int> q;
+    for(int i = 1 ; i <= n ; i++)
+    {
+        if(deg[i] == 0)
+            q.push(i);
+    }   
+    while(!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        for(int &v : g[u])
+        {
+            if(dp[u] > 0)
+                dp[v] = (dp[u] + dp[v]) % ((int)1e9 + 7);
+        }
+    }
+}
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int n, m;
     cin >> n >> m;
-    vector<vector<int>> g(n);
-    vector<int> deg(n + 1, 0);
+    dp[1] = 1;
     for(int i = 0 ; i < m ; i++)
     {
         int a, b;
@@ -18,5 +38,6 @@ int main()
         g[a].push_back(b);
         deg[b]++;
     }
+    bfs();
     return 0;
 }
