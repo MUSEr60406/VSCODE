8353f6d40881aa55f1ab6482c2c613025ee6d3f4
MUSEr60406 <sammy60406test@gmail.com>
2026-05-09 06:44:51 +0000

    2026年5月9日 下午2:44

---
+++ CSES/1132.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+vector<vector<int>> tree;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    tree.resize(n + 1);
+    for(int i = 0 ; i < n ; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+    return 0;
+}
