2811cd451e7bd61c043cf629ef262553cead923b
MUSEr60406 <sammy60406test@gmail.com>
2026-05-25 14:21:58 +0000

    2026年5月25日 下午10:21

--- hw5.cpp
+++ hw5.cpp
@@ -1,95 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <iomanip>
 using namespace std;
 
-class CarbonFootprint
+void function3(int x)
 {
-public:
-    virtual double getCarbonFootprint() const = 0;
-    virtual string getInfo() const = 0;
-    virtual ~CarbonFootprint() {} 
-};
-
-class Building : public CarbonFootprint
+    cout << "In function3\n";
+    throw x;
+}
+void function2(int x)
 {
-private:
-    string name;
-    double electricityUsage;
+    cout << "In function2\n";
+    function3(x);
+}
 
-public:
-    Building(string n, double e) : name(n), electricityUsage(e) {};
-    double getCarbonFootprint() const override
-    {
-        return electricityUsage * 0.5;
-    }
-    string getInfo() const override
-    {
-        return "Buliding " + name;
-    }
-};
-
-class Car : public CarbonFootprint
+void function1(int x)
 {
-private:
-    string brand;
-    double fuelUsed;
-
-public:
-    Car(string b, double f) : brand(b), fuelUsed(f) {};
-    double getCarbonFootprint() const override
-    {
-        return fuelUsed * 2.3;
-    }
-    string getInfo() const override
-    {
-        return "Car " + brand;
-    }
-};
-
-class Bicycle : public CarbonFootprint
-{
-private:
-    string brand;
-    double distance;
-
-public:
-    Bicycle(string b, double d) : brand(b), distance(d) {};
-    double getCarbonFootprint() const override
-    {
-        return 0.00;
-    }
-    string getInfo() const override
-    {
-        return "Bicycle " + brand;
-    }
-};
+    cout << "In function1\n";
+    function2(x);
+}
 
 int main()
 {
-    string buildingName, carBrand, bicycleBrand;
-    double electricityUsage, fuelUsed, distance;
+    int x;
+    cin >> x;
 
-    cin >> buildingName >> electricityUsage;
-    cin >> carBrand >> fuelUsed;
-    cin >> bicycleBrand >> distance;
-
-    Building *b = new Building(buildingName, electricityUsage);
-    Car *c = new Car(carBrand, fuelUsed);
-    Bicycle *bi = new Bicycle(bicycleBrand, distance);
-
-    vector<CarbonFootprint*> items;
-
-    items.push_back(b);
-    items.push_back(c);
-    items.push_back(bi);
-
-    for (CarbonFootprint* item : items)
+    try
     {
-        cout << item->getInfo() << ": "
-             << fixed << setprecision(2)
-             << item->getCarbonFootprint() << endl;
+        cout << "In main" << endl;
+        function1(x);
+    }
+    catch (int e)
+    {
+        cout << "Exception caught in main: " << e << endl;
     }
 
     return 0;
