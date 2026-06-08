5311f1790fbfc9c9cbded7aa5e0eb9d599f6c1f5
MUSEr60406 <sammy60406test@gmail.com>
2026-06-08 11:15:24 +0000

    2026年6月8日 下午7:15

--- UVa/10130.cpp
+++ UVa/10130.cpp
@@ -7,7 +7,23 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    int p[1005] = {0}, w[1005] = {0};
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int g, n, p[1005] = {0}, w[1005] = {0}, dp[35] = {0};
+        cin >> n;
+        for(int i = 0 ; i < n ; i++)
+            cin >> p[i] >> w[i];
+        cin >> g;
+        while(g--)
+        {
+            int max_w;
+            cin >> max_w;
+            memset(dp, 0, sizeof(dp));
+        }
+    }
+    
 
     return 0;
 }
