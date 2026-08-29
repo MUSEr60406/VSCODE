0a91d9f6d0d989bcc6611e0044df24c109cf2e1c
MUSEr60406 <sammy60406test@gmail.com>
2026-08-29 09:13:19 +0000

    2026年8月29日 下午5:13

--- test.cpp
+++ test.cpp
@@ -7,10 +7,37 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    ll n, m;
+    ll n, m, cities = 0;
     cin >> n >> m;
-    vector<bool> vis(n, false);
-    vector<int> cities;
-
+    vector<vector<ll>> M(n + 1);
+    vector<bool> vis(n + 1, false);
+    for(int i = 0 ; i < m ; i++)
+    {
+        ll a, b;
+        cin >> a >> b;
+        M[a].push_back(b);
+        M[b].push_back(a);
+    }
+    for(int i = 1 ; i <= n ; i++)
+    {
+        if(!vis[i])
+        {
+            vis[i] = true;
+            queue<ll> q;
+            q.push(i);
+            while(!q.empty())
+            {
+                for(ll &v : M[i])
+                {
+                    if(!vis[v])
+                    {
+                        q.push(v);
+                        vis[v] = true;
+                    }
+                }
+            }
+            cities++;
+        }
+    }
     return 0;
 }
