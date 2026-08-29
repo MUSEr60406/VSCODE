81a86d3d0169b2b7613b931b10352d88f294d531
MUSEr60406 <sammy60406test@gmail.com>
2026-08-29 09:21:04 +0000

    2026年8月29日 下午5:21

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ int main()
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     ll n, m;
     vector<vector<ll>> M(n + 1);
-    vector<bool> vis(n + 1);
+    vector<bool> vis(n + 1, false);
     vector<ll> fa(n + 1);
     for(int i = 0 ; i < m ; i++)
     {
@@ -17,6 +17,22 @@ int main()
         M[a].push_back(b);
         M[b].push_back(a);
     }
-
+    queue<ll> q;
+    q.push(1);
+    vis[1] = true;
+    while(!q.empty())
+    {
+        ll now = q.front();
+        q.pop();
+        for(ll &v : M[now])
+        {
+            if(!vis[v])
+            {
+                fa[v] = now;
+                vis[v] = true;
+                q.push(v);
+            }
+        }
+    }
     return 0;
 }
