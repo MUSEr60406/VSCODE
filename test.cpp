381d8984e555cff6717b694b37d8bfaa40b0154a
MUSEr60406 <sammy60406test@gmail.com>
2026-08-29 09:40:41 +0000

    2026年8月29日 下午5:40

--- test.cpp
+++ test.cpp
@@ -8,9 +8,10 @@ int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     ll n, m;
+    bool pos = true;
     cin >> n >> m;
     vector<vector<ll>> fri(n + 1);
-    vector<int> team(n + 1, 1);
+    vector<int> team(n + 1, 0);
     for(int i = 0 ; i < m ; i++)
     {
         ll a, b;
@@ -18,9 +19,35 @@ int main()
         fri[a].push_back(b);
         fri[b].push_back(a);
     }
-    queue<ll> q;
-    q.push(1);
-
-
+    for(int i = 1 ; i <= n ; i++)
+    {
+        if(team[i] == 0)
+        {
+            queue<ll> q;
+            q.push(i);
+            team[i] = 1;
+            while(!q.empty())
+            {
+                ll now = q.front();
+                q.pop();
+                for(ll &v : fri[now])
+                {
+                    if(team[v] == 0)
+                    {
+                        team[v] = (team[now] == 1 ? 2 : 1);
+                        q.push(v);
+                    }
+                    else if(team[now] == team[v])
+                        pos = false;
+                }
+            }
+        }
+    }
+    if(pos)
+        for(int i = 1 ; i <= n ; i++)
+            cout << team[i] << " ";
+    else
+        cout << "IMPOSSIBLE";
+    cout << "\n";
     return 0;
 }
