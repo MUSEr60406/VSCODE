b1b8aad0c1898a83451f7ed0f4bc71233d087ae7
MUSEr60406 <sammy60406test@gmail.com>
2026-08-30 12:06:07 +0000

    2026年8月30日 下午8:06

--- test.cpp
+++ test.cpp
@@ -3,20 +3,28 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-
+ll n, m;
+vector<vector<ll>> M(n + 1);
+vector<bool> vis(n + 1);
+vector<bool> stack(n + 1, false);
+vector<ll> path, cycle;
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    ll n, m;
     cin >> n >> m;
-    vector<vector<ll>> M(n + 1);
-    vector<bool> vis(n + 1);
-    vector<ll> stack;
     for(ll i = 0 ; i < m ; i++)
     {
         ll u, v;
         cin >> u >> v;
         M[u].push_back(v);
+    }
+    bool c = false;
+    for(ll i = 1 ; i <= n ; i++)
+    {
+        if(!vis[i])
+        {
+
+        }
     }
 
     return 0;
