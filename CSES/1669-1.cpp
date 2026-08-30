3ff8d5b9daca7793701cdc38616de6190d0b61fd
MUSEr60406 <sammy60406test@gmail.com>
2026-08-30 12:27:31 +0000

    2026年8月30日 下午8:27

---
+++ CSES/1669-1.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+ll n, m;
+vector<vector<ll>> M;
+vector<bool> vis;
+vector<bool> Stack;
+vector<ll> path, cycle;
+bool dfs(ll start, ll parent)
+{
+    vis[start] = true;
+    Stack[start] = true;
+    path.push_back(start);
+    for(ll &v : M[start])
+    {
+        if(v == parent)
+            continue;
+        if(!vis[v])
+        {
+            if(dfs(v, start))
+                return true;
+        }
+        else if(Stack[v])
+        {
+            auto it = find(path.begin(), path.end(), v);
+            cycle.assign(it, path.end());
+            return true;
+        }
+    }
+    Stack[start] = false;
+    path.pop_back();
+    return false;
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cin >> n >> m;
+    M.resize(n + 1);
+    vis.assign(n + 1, false);
+    Stack.assign(n + 1, false);
+    for(ll i = 0 ; i < m ; i++)
+    {
+        ll u, v;
+        cin >> u >> v;
+        M[u].push_back(v);
+        M[v].push_back(u);
+    }
+    bool c = false;
+    for(ll i = 1 ; i <= n ; i++)
+    {
+        if(!vis[i])
+        {
+            if(dfs(i, 0))
+            {
+                c = true;
+                break;
+            }
+        }
+    }
+    if(c)
+    {
+        cout << cycle.size() + 1 << "\n";
+        for(ll &node : cycle)
+            cout << node << " ";
+        cout << *cycle.begin() << "\n";
+    }
+    else
+        cout << "IMPOSSIBLE\n";
+    return 0;
+}
