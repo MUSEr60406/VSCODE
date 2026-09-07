67c873878a7644cdc091aa9750fe06645ccee92d
MUSEr60406 <sammy60406test@gmail.com>
2026-09-07 02:49:45 +0000

    2026年9月7日 上午10:49

--- test.cpp
+++ test.cpp
@@ -3,32 +3,30 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-ll n, m, q;
-ll adj[505][505], ans[505][505];
-void dij(ll start)
-{
-    dis[start][start] = 0;
-    vector<ll> dis(n + 1, LLONG_MAX / 2); 
-}
+vector<vector<ll>> M(505, {505, LLONG_MAX / 2});
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ll n, m, q;
     cin >> n >> m >> q;
     for(ll i = 0 ; i < m ; i++)
     {
         ll u, v, w;
         cin >> u >> v >> w;
-        adj[u][v] = min(adj[u][v], w);
-        adj[v][u] = min(adj[v][u], w);
+        M[u][v] = M[v][u] = min(M[u][v], w);
     }
-    for(ll i = 1 ; i <= 500 ; i++)
-        dij(i);
+    for(ll k = 1 ; k <= n ; k++)
+        for(ll i = 1 ; i <= n ; i++)
+            for(ll j = 1 ; j <= n ; j++)
+                M[i][j] = min(M[i][j], M[i][k] + M[k][j]);
     for(ll i = 0 ; i < q ; i++)
     {
-        ll start, end;
-        cin >> start >> end;
-      
+        ll u, v;
+        cin >> u >> v;
+        if(M[u][v] >= LLONG_MAX / 2)
+            cout << "-1\n";
+        else
+            cout << M[u][v] << "\n";
     }
-
     return 0;
 }
