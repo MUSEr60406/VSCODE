7a0305fed02eccde50f0742322b10495440a7d07
MUSEr60406 <sammy60406test@gmail.com>
2026-09-07 02:32:14 +0000

    2026年9月7日 上午10:32

--- test.cpp
+++ test.cpp
@@ -5,7 +5,29 @@
 using namespace std;
 ll n, m, q;
 vector<vector<pll>> M(505, vector<pll>(505));
-vector<vector<ll>> vis(505, {505, 0});
+vector<ll> vis(505);
+void dij(ll start, ll end)
+{
+    vis.assign(505, LLONG_MAX/2);
+    vis[start] = 0;
+    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    pq.push({0, start});
+    while(!pq.empty())
+    {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if(d > vis[u]) 
+            continue;
+        for(auto &[w, v] : M[u])
+        {
+            if(vis[u] + w < vis[v])
+            {
+                vis[v] = vis[u] + w;
+                pq.push({vis[v], v});
+            }
+        }
+    }
+}
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
