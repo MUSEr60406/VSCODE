a8cae66c7007f0e1805169489de9575ef5e4fabb
MUSEr60406 <sammy60406test@gmail.com>
2026-09-07 02:51:49 +0000

    2026年9月7日 上午10:51

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #define pll pair<long,long>
 #define ll long long
 using namespace std;
-vector<vector<ll>> M(505, {505, LLONG_MAX / 2});
+vector<vector<ll>> M(505, vector<ll>(505, LLONG_MAX / 2));
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
