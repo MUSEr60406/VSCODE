75abe9942115da03102da43a893f03bea15ddec3
MUSEr60406 <sammy60406test@gmail.com>
2026-09-07 05:11:22 +0000

    2026年9月7日 下午1:11

---
+++ 2024ICPC TW/pL.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, m, s, t, x, k;
+    cin >> n >> m >> s >> t >> x >> k;
+
+    return 0;
+}
