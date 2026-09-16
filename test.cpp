133d9519598703d8827a2c2cc048c5b52acf84a6
MUSEr60406 <sammy60406test@gmail.com>
2026-09-16 10:00:56 +0000

    2026年9月16日 下午6:00

--- test.cpp
+++ test.cpp
@@ -10,41 +10,6 @@ int main()
     ll n, m;
     cin >> n >> m;
     vector<vector<pll>> M(n + 1);
-    vector<ll> dis(n + 1, -LLONG_MAX / 2);
-    for(int i = 0 ; i < m ; i++)
-    {   
-        ll u, v, w;
-        cin >> u >> v >> w;
-        M[u].push_back({w, v});
-    }
-    dis[1] = 0;
-    for(int ii = 1 ; ii <= n - 1 ; ii++) //bellman
-    {
-        for(int i = 1 ; i <= n ; i++)
-        {
-            for(auto &[w, v] : M[i])
-            {
-                if(dis[i] > -LLONG_MAX / 2)
-                {
-                    if(dis[i] + w > dis[v])
-                        dis[v] = dis[i] + w;
-                }   
-            }
-        }
-    }
-    for(int ii = 1 ; ii <= n ; ii++)
-    {
-        for(int i = 1 ; i <= n ; i++)
-        {
-            for(auto &[w, v] : M[i])
-            {
-                if(dis[i] <= -LLONG_MAX / 2)
-                    continue;
-                if(dis[i] >= LLONG_MAX / 2 || dis[i] + w > dis[v])
-                    dis[v] = LLONG_MAX / 2;
-            }
-        }
-    }
-    cout << (dis[n] >= LLONG_MAX / 2 ? -1 : dis[n]) << "\n";
+
     return 0;
 }
