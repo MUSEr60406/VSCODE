3c0306fa62c55e7183c2c197b05c052af563eeb8
MUSEr60406 <sammy60406test@gmail.com>
2026-09-16 16:02:33 +0000

    2026年9月17日 上午12:02

--- test.cpp
+++ test.cpp
@@ -10,18 +10,19 @@ int main()
     ll n, m;
     cin >> n >> m;
     vector<vector<pll>> M(n + 1);
-    vector<ll> dis(n + 1, 0);
+    vector<ll> dis(n + 1, 0); // 初
     vector<ll> fa(n + 1, 0);
-    vector<ll> cycle;
     for(ll i = 0 ; i < m ; i++)
     {
         ll u, v, w;
         cin >> u >> v >> w;
         M[u].push_back({v, w});
     }
-    for(ll u = 1 ; u <= n ; u++)
+    ll x = -1; 
+    for(ll i = 0 ; i < n ; i++)
     {
-        for(ll i = 0 ; i < n - 1 ; i++)
+        x = -1; 
+        for(ll u = 1 ; u <= n ; u++)
         {
             for(auto &[v, w] : M[u])
             {
@@ -29,36 +30,40 @@ int main()
                 {
                     dis[v] = dis[u] + w;
                     fa[v] = u;
+                    x = v; 
                 }
             }
         }
     }
-    for(ll u = 1 ; u <= n ; u++)
+    if(x == -1)
+        cout << "NO\n";
+    else
     {
-        for(auto &[v, w] : M[u])
+        for(ll i = 0 ; i < n ; i++)
         {
-            if(dis[u] + w < dis[v])
+            x = fa[x];
+        }
+        vector<ll> cycle;
+        ll curr = x;
+
+        while(true)
+        {
+            cycle.push_back(curr);
+            curr = fa[curr];
+            if(curr == x && cycle.size() > 1) 
             {
-                ll node = v, start;
-                for(ll i = 0 ; i < n ; i++)
-                {
-                    start = fa[node];
-                    node = start;
-                }
-                ll now = fa[start];
-                cycle.push_back(start);
-                while(now != start)
-                {
-                    cycle.push_back(now);
-                    now = fa[now];
-                }
-                cout << "YES\n";
-                for(ll &i : cycle)
-                    cout << i << " ";
-                return 0;
+                cycle.push_back(curr);
+                break;
             }
         }
+        reverse(cycle.begin(), cycle.end());
+        cout << "YES\n";
+        for(ll &node : cycle)
+        {
+            cout << node << " ";
+        }
+        cout << "\n";
     }
-    cout << "NO\n";
+    
     return 0;
 }
