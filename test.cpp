68d9bca4eb2eb0097e89174b8044859957c675e5
MUSEr60406 <sammy60406test@gmail.com>
2026-09-16 15:39:25 +0000

    2026年9月16日 下午11:39

--- test.cpp
+++ test.cpp
@@ -11,6 +11,8 @@ int main()
     cin >> n >> m;
     vector<vector<pll>> M(n + 1);
     vector<ll> dis(n + 1, 0);
+    vector<ll> fa(n + 1, 0);
+    vector<ll> cycle;
     for(ll i = 0 ; i < m ; i++)
     {
         ll u, v, w;
@@ -21,7 +23,14 @@ int main()
     {
         for(ll j = 1 ; j <= n ; j++)
         {
-            
+            for(auto &[v, w] : M[j])
+            {
+                if(dis[j] + w < dis[v])
+                {
+                    dis[v] = dis[j] + w;
+                    fa[v] = j;
+                }
+            }
         }
     }
 
